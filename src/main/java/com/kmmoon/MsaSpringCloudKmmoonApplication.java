35c1d7eb93787a0fd5b44cc41d8f4fe63e4d83dc
nemesis1001 <nemesis1001@naver.com>
2022-02-24 02:22:04 +0900

    첫 프로젝트 생성

---
+++ src/main/java/com/kmmoon/MsaSpringCloudKmmoonApplication.java
@@ -0,0 +1,13 @@
+package com.kmmoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaSpringCloudKmmoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaSpringCloudKmmoonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kmmoon/MsaSpringCloudKmmoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmmoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaSpringCloudKmmoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
